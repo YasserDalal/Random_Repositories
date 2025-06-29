d028a1cadb67c260a19729a3d563c95c5155f4cb
YasserDalal <170532228+YasserDalal@users.noreply.github.com>
2025-06-29 20:32:40 +0300

    create a Header component

---
+++ src/layouts/Header.jsx
@@ -0,0 +1,16 @@
+import GithubLogo from '../assets/github-mark-white.png'
+
+export default function Header({ className }) {
+  return (
+    <div className={className}>
+      <div className='flex gap-5 items-center text-white pt-10 px-10 max-[440px]:px-4'>
+        <div className='w-14'>
+          <img src={GithubLogo} alt="github logo"/>
+        </div>
+        <div>
+          <h1 className='font-semibold'>Github Random Repository</h1>
+        </div>
+      </div>
+    </div>
+  )
+}

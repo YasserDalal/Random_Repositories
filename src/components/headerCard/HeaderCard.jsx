69b6a3c1876e2ede0b5eb75201e9793355d588e9
YasserDalal <170532228+YasserDalal@users.noreply.github.com>
2025-06-30 23:08:59 +0300

    create HeaderCard component

---
+++ src/components/headerCard/HeaderCard.jsx
@@ -0,0 +1,11 @@
+
+export default function HeaderCard() {
+  return (
+    <div className='bg-transparent '>
+      <div className='flex gap-4 items-center'>
+        <div className='border border-white w-14 h-12 rounded-lg'></div>
+        <div className='text-3xl font-semibold'>Repository Finder</div>
+      </div>
+    </div>
+  )
+} 

99675df3c1bba5e1e8af6a2179d599e5f290cca4
YasserDalal <170532228+YasserDalal@users.noreply.github.com>
2025-06-30 23:26:57 +0300

    import the Center/Card components in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 
 import Header from './layouts/Header'
+import Center from './layouts/Center'
+import Card from './layouts/Card' 
 
 export default function App() {
   return (
-    <div className='bg-[#101010] h-screen min-w-[320px] max-w-full w-full'> {/* after finishing the UI change the h-screen to h-auto*/}
-      <Header className="bg-[#101010] text-[clamp(10px,5.29vw,30px)] max-[320px]:text-[17px] "/>
+    <div className='bg-[#101010] h-screen min-w-[320px] max-w-full w-full flex flex-col gap-28'> {/* after finishing the UI change the h-screen to h-auto*/}
+      <Header className="bg-[#101010] text-[clamp(10px,5.29vw,36px)] max-[320px]:text-[17px] "/>
+      <Center className='bg-[#101010] '>
+        <Card />
+      </Center>
     </div>
   )
 }

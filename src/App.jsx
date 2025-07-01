ddbd4ab3fe91de45703eafab9ab6b1b9e29677c0
YasserDalal <170532228+YasserDalal@users.noreply.github.com>
2025-07-01 18:51:01 +0300

    import the Footer/Watermark components inside the App component

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,18 @@
 import Header from './layouts/Header'
 import Center from './layouts/Center'
 import Card from './layouts/Card' 
-
+import Footer from './layouts/Footer'
+import Watermark from './components/Watermark/Watermark'  
 export default function App() {
   return (
-    <div className='bg-[#101010] h-screen min-w-[320px] max-w-full w-full flex flex-col gap-28'> {/* after finishing the UI change the h-screen to h-auto*/}
+    <div className='bg-[#101010] h-auto min-w-[320px] max-w-full w-full flex flex-col gap-28 max-[768px]:gap-20'>
       <Header className="bg-[#101010] text-[clamp(10px,5.29vw,36px)] max-[320px]:text-[17px] "/>
       <Center className='bg-[#101010] '>
         <Card />
       </Center>
+      <Footer>
+        <Watermark className='flex flex-col justify-center items-center text-[#ababab] pb-3 select-none'/>
+      </Footer>
     </div>
   )
 }

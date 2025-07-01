3e168fa2e51201f773cd6dd0163b86d4ca5f3de7
YasserDalal <170532228+YasserDalal@users.noreply.github.com>
2025-07-01 18:26:07 +0300

    make the Card component responsive

--- src/components/success/Stats/Stats.jsx
+++ src/components/success/Stats/Stats.jsx
@@ -4,7 +4,7 @@ import Forks from './StatsComponents/Forks'
 import Shields from './StatsComponents/Shields'
 export default function Stats() {
   return (
-    <div className='flex gap-4'>
+    <div className='flex justify-between max-[400px]:justify-around'>
       <Language />
       <Stars />
       <Forks />
--- src/layouts/Card.jsx
+++ src/layouts/Card.jsx
@@ -6,7 +6,7 @@ import RetryButton from '../components/error/button/RetryButton'
 
 export default function Card() {
   return (
-    <div className='flex flex-col gap-6 text-white min-w-min max-w-[320px] w-full'>
+    <div className='flex flex-col gap-6 text-white min-w-min max-w-[340px] max-[400px]:min-w-none max-[400px]:max-w-none w-full'>
       <HeaderCard />
       <Dropdown /> 
       <Display/>

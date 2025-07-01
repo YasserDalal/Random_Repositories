2701c4a71d78756514b36865d586d33f1709c17f
YasserDalal <170532228+YasserDalal@users.noreply.github.com>
2025-07-01 20:58:02 +0300

    add dark effects to the Display component

--- src/components/Display.jsx
+++ src/components/Display.jsx
@@ -3,9 +3,9 @@ import Loading from './loading/Loading'
 import Repository from './success/Repository' 
 import Error from './error/Error'
 
-export default function Display() {
+export default function Display({ open }) {
   return (
-    <div className='flex flex-col gap-6 text-black h-full'>
+    <div className={`first-letter:flex flex-col gap-6 pb-6 text-black h-full ${open && 'brightness-[.4]'}`}>
       {/* remove the comments to see the components */}
 
       {/*<DefaultDisplay />*/}

c3c15a63adeedf8594111771d3e2797a76a9c391
YasserDalal <170532228+YasserDalal@users.noreply.github.com>
2025-07-01 20:24:56 +0300

    render the List component inside the Dropdown component

--- src/components/dropdownLists/Dropdown.jsx
+++ src/components/dropdownLists/Dropdown.jsx
@@ -4,10 +4,10 @@ import { faCaretDown} from '@fortawesome/free-solid-svg-icons'
 
 const defaultText = 'Select a Language'
 
-export default function Dropdown() {
+export default function Dropdown({ children }) {
   return (
-    <div className='border border-white rounded-lg py-2 cursor-pointer hover:brightness-75 transition ease-in-out duration-100' typeof='button'>
-      <div className='flex justify-between px-4'>
+    <div className='relative pb-6' typeof='button'>
+      <div className='bg-[#101010] flex justify-between px-4 border border-white rounded-lg py-2 cursor-pointer hover:brightness-75 transition ease-in-out duration-100 z-50 relative'>
         <div className='text-[20px] font-semibold'>
           {/*defaultText*/}
           Javascript
@@ -15,6 +15,9 @@ export default function Dropdown() {
         <div>
           <FontAwesomeIcon icon={faCaretDown} size='2xl' className='bg-transparent'/>
         </div>
+      </div>
+      <div className='absolute bg-[#070707] border-x-2 border-b-2 border-[#303030] text-[20px] font-semibold z-40 right-0 left-0 top-[50px] shadow-[#777777] shadow-[0px_15px_20px_-6px] rounded-b-lg overflow-hidden -translate-y-32'>
+        {children}
       </div>
     </div>
   )
--- src/layouts/Card.jsx
+++ src/layouts/Card.jsx
@@ -1,16 +1,32 @@
 import HeaderCard from '../components/headerCard/headerCard'  
 import Dropdown from '../components/dropdownLists/Dropdown'
+import Lists from '../components/dropdownLists/Lists' 
 import Display from '../components/Display'
 import RefreshButton from '../components/success/Button/RefreshButton'
 import RetryButton from '../components/error/button/RetryButton'
 
 export default function Card() {
   return (
-    <div className='flex flex-col gap-6 text-white min-w-min max-w-[340px] max-[400px]:min-w-none max-[400px]:max-w-none w-full'>
+    <div className='flex flex-col text-white min-w-min max-w-[340px] max-[400px]:min-w-none max-[400px]:max-w-none w-full'>
       <HeaderCard />
-      <Dropdown /> 
+
+      <Dropdown>
+        <Lists className='px-9 py-2 border-b-[1px] text-[17px] border-[#6d6d6d] cursor-pointer hover:bg-[#272727]'>
+          Javascript
+        </Lists>
+        <Lists className='px-9 py-2 border-b-[1px] text-[17px] border-[#6d6d6d] cursor-pointer hover:bg-[#272727]'>
+          Javascript
+        </Lists>
+        <Lists className='px-9 py-2 border-b-[1px] text-[17px] border-[#6d6d6d] cursor-pointer hover:bg-[#272727]'>
+          Javascript
+        </Lists>
+        <Lists className='px-9 py-2 cursor-pointer text-[17px] hover:bg-[#272727]'>
+          Javascript
+        </Lists>
+      </Dropdown> 
+
       <Display/>
-      
+    
       {/* remove the comments to see the button */}
       {<RefreshButton />}  {/*<RetryButton />*/}   
     </div>

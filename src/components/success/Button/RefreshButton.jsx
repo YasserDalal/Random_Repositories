b94f320f3e89ab30561def104da75b9a99e7020a
YasserDalal <170532228+YasserDalal@users.noreply.github.com>
2025-07-02 21:01:40 +0300

    fetch Github API for repositories in a random order

--- src/components/success/Button/RefreshButton.jsx
+++ src/components/success/Button/RefreshButton.jsx
@@ -2,8 +2,14 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faRotate } from '@fortawesome/free-solid-svg-icons'
 
 export default function RefreshButton() {
+  const fetchRepository = () => {
+    fetch('https://api.github.com/users/YasserDalal/repos')
+    .then((response) => response.json())
+      .then((data) => 
+        console.log(data[Math.floor(Math.random() * data.length)]))
+  }
   return (
-    <div className='border border-white py-3 px-4 rounded-lg flex justify-center items-center gap-2 cursor-pointer hover:scale-105 hover:bg-[#2a2a2a] hover:border-[#2a2a2a] transition ease-in-out duration-100' typeof='button'>
+    <div className='border border-white py-3 px-4 rounded-lg flex justify-center items-center gap-2 cursor-pointer hover:scale-105 hover:bg-[#2a2a2a] hover:border-[#2a2a2a] transition ease-in-out duration-100' typeof='button' onClick={fetchRepository}>
       <FontAwesomeIcon icon={faRotate} size='md' className='bg-transparent'/>
       <div className='select-none text-xl'>Refresh</div>
     </div>

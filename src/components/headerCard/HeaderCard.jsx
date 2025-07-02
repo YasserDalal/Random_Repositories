b6519bcb6df2193c81770fc8e13f0f55349fe717
YasserDalal <170532228+YasserDalal@users.noreply.github.com>
2025-07-02 19:04:40 +0300

    fix a bug, when we clicked the Dropdown there is a text rising up behind during the animation

--- src/components/headerCard/HeaderCard.jsx
+++ src/components/headerCard/HeaderCard.jsx
@@ -1,7 +1,7 @@
 
 export default function HeaderCard() {
   return (
-    <div className='bg-transparent '>
+    <div className='bg-[#101010] pb-6 pt-4 relative z-50'>
       <div className='flex gap-4 items-center'>
         <div className='border border-white w-14 h-12 rounded-lg'></div>
         <div className='text-3xl font-semibold'>Repository Finder</div>

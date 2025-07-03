711603a3183e623e4cd365b1e32e19fe0354f3bf
YasserDalal <170532228+YasserDalal@users.noreply.github.com>
2025-07-03 22:29:53 +0300

    render the Modal component inside the App component

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,18 @@ import Center from './layouts/Center'
 import Card from './layouts/Card' 
 import Footer from './layouts/Footer'
 import Watermark from './components/Watermark/Watermark' 
+import Modal from './layouts/Modal'
 
 export default function App() {
   const [open, setOpen] = useState(false)
   const [userName, setUserName] = useState('YasserDalal') // change this in your GitHub username
   const [data, setData] = useState()
-  
+  const [openModal, setOpenModal] = useState(true)
   useEffect(() => {
     console.log(data) // check if the data is available
   }, [data])
   return (
-    <div className='bg-[#101010] h-auto min-w-[320px] max-w-full w-full flex flex-col'>
+    <div className='bg-[#101010] h-auto min-w-[320px] max-w-full w-full flex flex-col relative'>
       <Header className="bg-[#101010] text-[clamp(10px,5.29vw,36px)] max-[320px]:text-[17px] pb-24 max-[768px]:pb-20" onClick={() => setOpen(false)}/>
       <Center className='bg-[#101010] flex justify-center items-center pb-24 max-[768px]:pb-20' onClick={() => open && setOpen(false)}>
         <Card className='flex flex-col text-white min-w-min max-w-[340px] max-[400px]:min-w-none max-[400px]:max-w-none w-full' open={open} setOpen={setOpen} data={data} setData={setData} userName={userName}/>
@@ -23,6 +24,13 @@ export default function App() {
       <Footer onClick={() => setOpen(false)}>
         <Watermark className='flex flex-col justify-center items-center text-[#ababab] pb-3 select-none'/>
       </Footer>
+
+      {/* Modal is the first thing that users will see */}
+      {openModal && (
+        <div className='absolute top-0 right-0 bottom-0 left-0 px-4'>
+          <Modal className='opacity-100 bg-[#101010] rounded-xl max-w-[500px] min-w-[300px] w-full h-96 left-1/2 top-[40%] -translate-x-1/2 -translate-y-1/2 relative z-50 shadow-[0.5px_3px_20px_0px] shadow-[#717171]' setOpenModal={setOpenModal}/>
+        </div>
+      )}
     </div>
   )
 }

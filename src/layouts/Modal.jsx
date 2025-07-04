802bf8e1d12264c833b1376d169ceac5827e0025
YasserDalal <170532228+YasserDalal@users.noreply.github.com>
2025-07-04 22:42:13 +0300

    create SubmitButton component for Modal component

--- src/layouts/Modal.jsx
+++ src/layouts/Modal.jsx
@@ -3,6 +3,7 @@ import { useState } from 'react'
 import Heading from '../components/Modal/Heading'
 import Input from '../components/Modal/Input'
 import Warning from '../components/Modal/Warning'
+import SubmitButton from '../components/Modal/SubmitButton'
 
 export default function Modal({ className, setFoundUserName, foundUserName, setOpenModal, setUserName, userName }) {
   const [placeholder, setPlaceholder] = useState(true)
@@ -22,6 +23,7 @@ export default function Modal({ className, setFoundUserName, foundUserName, setO
         <Heading className='flex justify-center pt-10 pb-5'/>
         <Input placeholder={placeholder} setPlaceholder={setPlaceholder} setUserName={setUserName} handleClickSubmit={handleClickSubmit} showWarning={showWarning} setShowWarning={setShowWarning} setFoundUserName={setFoundUserName}/>
         <Warning showWarning={showWarning} userName={userName} foundUserName={foundUserName}/>
+        <SubmitButton className='flex justify-center px-5 pb-5' setOpenModal={setOpenModal} setUserName={setUserName} handleClickSubmit={handleClickSubmit} userName={userName} setData={setData} setShowWarning={setShowWarning} setFoundUserName={setFoundUserName}/>
       </div>
     </div>
   )

4d130c8547b839c62618952c157ec8999af9df38
YasserDalal <170532228+YasserDalal@users.noreply.github.com>
2025-07-04 22:45:17 +0300

    create GuestInfo component for Modal component

---
+++ src/components/Modal/GuestInfo.jsx
@@ -0,0 +1,9 @@
+
+export default function GuestInfo({ className }) {
+  return (
+    <div className={className}>
+      <div>If you don't have a Github username, kindly</div>
+      <div className='font-bold'>Continue as guest</div>
+    </div>
+  )
+}
--- src/layouts/Modal.jsx
+++ src/layouts/Modal.jsx
@@ -4,6 +4,7 @@ import Heading from '../components/Modal/Heading'
 import Input from '../components/Modal/Input'
 import Warning from '../components/Modal/Warning'
 import SubmitButton from '../components/Modal/SubmitButton'
+import GuestInfo from '../components/Modal/GuestInfo'
 
 export default function Modal({ className, setFoundUserName, foundUserName, setOpenModal, setUserName, userName }) {
   const [placeholder, setPlaceholder] = useState(true)
@@ -24,6 +25,7 @@ export default function Modal({ className, setFoundUserName, foundUserName, setO
         <Input placeholder={placeholder} setPlaceholder={setPlaceholder} setUserName={setUserName} handleClickSubmit={handleClickSubmit} showWarning={showWarning} setShowWarning={setShowWarning} setFoundUserName={setFoundUserName}/>
         <Warning showWarning={showWarning} userName={userName} foundUserName={foundUserName}/>
         <SubmitButton className='flex justify-center px-5 pb-5' setOpenModal={setOpenModal} setUserName={setUserName} handleClickSubmit={handleClickSubmit} userName={userName} setData={setData} setShowWarning={setShowWarning} setFoundUserName={setFoundUserName}/>
+        <GuestInfo className='text-gray-400 flex gap-1 text-nowrap justify-center text-[clamp(10.3px,2.81vw,15px)] px-5 pb-5 max-[336px]:text-[10.3px] max-[336px]:px-3'/>
       </div>
     </div>
   )

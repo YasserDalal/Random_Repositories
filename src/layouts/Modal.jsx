4821d9e25ba89b2f68e4f4d3104c7d3ec11be997
YasserDalal <170532228+YasserDalal@users.noreply.github.com>
2025-07-06 19:52:10 +0300

    adjust the structure of components in Modal component

--- src/layouts/Modal.jsx
+++ src/layouts/Modal.jsx
@@ -44,12 +44,37 @@ export default function Modal({ className, setFoundUserName, foundUserName, setO
   return (
     <div className={className}>
       <div className='flex flex-col text-[#eaeaea]'>
-        <Heading className='flex justify-center pt-10 pb-5'/>
-        <Input placeholder={placeholder} setPlaceholder={setPlaceholder} setUserName={setUserName} fetchRepository={fetchRepository} showWarning={showWarning} setShowWarning={setShowWarning} setFoundUserName={setFoundUserName}/>
-        <Warning showWarning={showWarning} userName={userName} foundUserName={foundUserName} noRepos={noRepos}/>
-        <SubmitButton className='flex justify-center px-5 pb-5' setOpenModal={setOpenModal} setUserName={setUserName} userName={userName} setData={setData} setShowWarning={setShowWarning} setFoundUserName={setFoundUserName} fetchRepository={fetchRepository}/>
+        <Heading className='flex justify-center pt-10 pb-14'/>
+
+        <Input 
+        placeholder={placeholder} 
+        setPlaceholder={setPlaceholder} 
+        setUserName={setUserName} 
+        fetchRepository={fetchRepository} 
+        showWarning={showWarning} 
+        setShowWarning={setShowWarning} 
+        setFoundUserName={setFoundUserName}/>
+
+        <Warning 
+        showWarning={showWarning} 
+        userName={userName} 
+        foundUserName={foundUserName} 
+        noRepos={noRepos}/>
+
+        <SubmitButton className='flex justify-center px-5 pb-5' 
+        setOpenModal={setOpenModal} 
+        setUserName={setUserName} 
+        userName={userName} 
+        setData={setData} 
+        setShowWarning={setShowWarning} 
+        setFoundUserName={setFoundUserName} 
+        fetchRepository={fetchRepository}/>
+        
         <GuestInfo className='text-gray-400 flex gap-1 text-nowrap justify-center text-[clamp(10.3px,2.81vw,15px)] px-5 pb-5 max-[336px]:text-[10.3px] max-[336px]:px-3'/>
-        <GuestButton className='flex justify-center px-5' setUserName={setUserName} setOpenModal={setOpenModal}/>
+
+        <GuestButton className='flex justify-center px-5' 
+        setUserName={setUserName} 
+        setOpenModal={setOpenModal}/>
       </div>
     </div>
   )

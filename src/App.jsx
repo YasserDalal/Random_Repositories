a824450e48acc4ee4be12401e98c9506ced9e468
YasserDalal <170532228+YasserDalal@users.noreply.github.com>
2025-07-08 20:58:23 +0300

    fix the code structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,78 +1,109 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect } from "react";
 
-import Header from './layouts/Header'
-import Center from './layouts/Center'
-import Card from './layouts/Card' 
-import Footer from './layouts/Footer'
-import Watermark from './components/Watermark/Watermark' 
-import Modal from './layouts/Modal'
-import LoadSignIn from './components/loading/LoadSignIn'
+import Header from "./layouts/Header";
+import Center from "./layouts/Center";
+import Card from "./layouts/Card";
+import Footer from "./layouts/Footer";
+import Watermark from "./components/Watermark/Watermark";
+import Modal from "./layouts/Modal";
+import LoadSignIn from "./components/loading/LoadSignIn";
 
 export default function App() {
-  const [open, setOpen] = useState(false)
-  const [userName, setUserName] = useState('')
-  const [data, setData] = useState()
-  const [openModal, setOpenModal] = useState(true)
-  const [foundUserName, setFoundUserName] = useState(false)
-  const [noRepos, setNoRepos] = useState(true)
-  const [prevName, setPrevName] = useState('') // for checking if the username is the same as the previous one
-  const [loading, setLoading] = useState(false) 
-  const [loadWidth, setLoadWidth] = useState(0)
-  const [showBar, setShowBar] = useState(false)
+  const [open, setOpen] = useState(false);
+  const [userName, setUserName] = useState("");
+  const [data, setData] = useState();
+  const [openModal, setOpenModal] = useState(true);
+  const [foundUserName, setFoundUserName] = useState(false);
+  const [noRepos, setNoRepos] = useState(true);
+  const [prevName, setPrevName] = useState(""); // for checking if the username is the same as the previous one
+  const [loading, setLoading] = useState(false);
+  const [loadWidth, setLoadWidth] = useState(0);
+  const [showBar, setShowBar] = useState(false);
 
   useEffect(() => {
-    console.log(data) // check if the data is available
-  }, [data])
+    console.log(data); // check if the data is available
+  }, [data]);
   useEffect(() => {
     // if the Modal is open, add overflow-hidden to the body to prevent scrolling
-    openModal ? document.body.classList.add('overflow-hidden') : document.body.classList.remove('overflow-hidden')
-  }, [openModal])
+    openModal
+      ? document.body.classList.add("overflow-hidden")
+      : document.body.classList.remove("overflow-hidden");
+  }, [openModal]);
   useEffect(() => {
     // variables for setInterval and setTimeout functions
     let interval;
     let timeout;
-    
+
     // Loading starts
     if (loading) {
       // show the loading bar
-      setShowBar(true)
+      setShowBar(true);
       // add 1% to loadWidth every 10ms
       interval = setInterval(() => {
-        setLoadWidth(prevWidth => prevWidth + 1)
+        setLoadWidth((prevWidth) => prevWidth + 1);
       }, 10);
-      return () => clearInterval(interval) // clean up
-    } 
+      return () => clearInterval(interval); // clean up
+    }
 
     // Loading ends
-    if(!loading) {
+    if (!loading) {
       // wait 1ms before hiding the loading bar
       timeout = setTimeout(() => {
-        setShowBar(false)
-        setLoadWidth(0)
-      }, 1)
-      return () => clearTimeout(timeout) // clean up
+        setShowBar(false);
+        setLoadWidth(0);
+      }, 1);
+      return () => clearTimeout(timeout); // clean up
     }
-    
-  }, [loading, data])
+  }, [loading, data]);
   return (
     <div className='bg-[#101010] min-h-screen min-w-[320px] max-w-full w-full flex flex-col relative'>
-      <Header className={`${openModal && 'opacity-40 blur-[2px]'} bg-[#101010] text-[clamp(10px,5.29vw,36px)] max-[320px]:text-[17px] pb-24 max-[768px]:pb-20`} onClick={() => setOpen(false)}/>
-      <Center className={`${openModal && 'opacity-40 blur-[2px]'} bg-[#101010] flex justify-center items-center pb-24 max-[768px]:pb-20`} onClick={() => open && setOpen(false)}>
-        <Card className={`${openModal && 'opacity-40 blur-[2px]'} flex flex-col text-white min-w-min max-w-[340px] max-[400px]:min-w-none max-[400px]:max-w-none w-full`} open={open} setOpen={setOpen} data={data} setData={setData} userName={userName}/>
+      <Header className={`${ openModal && "opacity-40 blur-[2px]"} 
+      bg-[#101010] text-[clamp(10px,5.29vw,36px)] max-[320px]:text-[17px] pb-24 max-[768px]:pb-20`}
+      onClick={() => setOpen(false)}/>
+
+      <Center className={`${ openModal && "opacity-40 blur-[2px]" } 
+        bg-[#101010] flex justify-center items-center pb-24 max-[768px]:pb-20`}
+        onClick={() => open && setOpen(false)}
+      >
+        <Card className={`${ openModal && "opacity-40 blur-[2px]" } 
+        flex flex-col text-white min-w-min max-w-[340px] max-[400px]:min-w-none max-[400px]:max-w-none w-full`}
+        open={open}
+        setOpen={setOpen}
+        data={data}
+        setData={setData}
+        userName={userName}/>
       </Center>
-      <Footer className={`${openModal && 'opacity-40 blur-[2px]'}`} onClick={() => setOpen(false)}>
-        <Watermark className='flex flex-col justify-center items-center text-[#ababab] pb-3 select-none'/>
+
+      <Footer className={`${openModal && "opacity-40 blur-[2px]"}`}
+        onClick={() => setOpen(false)}>
+        <Watermark className='flex flex-col justify-center items-center text-[#ababab] pb-3 select-none' />
       </Footer>
 
       {/* Modal is the first thing that users will see */}
       {openModal && (
         <div className='fixed top-0 right-0 bottom-0 left-0 px-4 max-[336px]:px-2'>
-          <Modal className='opacity-100 bg-[#101010] rounded-xl max-w-[500px] min-w-[300px] w-full pb-7 left-1/2 top-[43%] -translate-x-1/2 -translate-y-1/2 relative z-50 shadow-[0.5px_3px_20px_0px] shadow-[#717171]' setOpenModal={setOpenModal} setUserName={setUserName} foundUserName={foundUserName} setFoundUserName={setFoundUserName} userName={userName} setData={setData} noRepos={noRepos} setNoRepos={setNoRepos} setLoading={setLoading} setPrevName={setPrevName} prevName={prevName} setLoadWidth={setLoadWidth}/>
-          <LoadSignIn className='absolute top-0 left-0 z-40 w-full'
-          showBar={showBar}
-          loadWidth={loadWidth}/>
+          <Modal
+            className='opacity-100 bg-[#101010] rounded-xl max-w-[500px] min-w-[300px] w-full pb-7 left-1/2 top-[43%] -translate-x-1/2 -translate-y-1/2 relative z-50 shadow-[0.5px_3px_20px_0px] shadow-[#717171]'
+            setOpenModal={setOpenModal}
+            setUserName={setUserName}
+            foundUserName={foundUserName}
+            setFoundUserName={setFoundUserName}
+            userName={userName}
+            setData={setData}
+            noRepos={noRepos}
+            setNoRepos={setNoRepos}
+            setLoading={setLoading}
+            setPrevName={setPrevName}
+            prevName={prevName}
+            setLoadWidth={setLoadWidth}
+          />
+          <LoadSignIn
+            className='absolute top-0 left-0 z-40 w-full'
+            showBar={showBar}
+            loadWidth={loadWidth}
+          />
         </div>
       )}
     </div>
-  )
+  );
 }

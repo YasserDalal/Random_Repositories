94ca81bf4ff07a34fddde3d4a51e5be55f4dcdf0
YasserDalal <170532228+YasserDalal@users.noreply.github.com>
2025-07-13 20:09:19 +0300

    fix a bug when rendering language colors

--- src/components/success/Stats/StatsComponents/Language.jsx
+++ src/components/success/Stats/StatsComponents/Language.jsx
@@ -1,9 +1,8 @@
 
 export default function Language({ randomRepo, languageColors }) {
-
   return (
     <div className='text-gray-600 flex gap-1 items-center'>
-      <div className='w-4 h-4 rounded-full' style={{ backgroundColor: languageColors ? languageColors[randomRepo.language].color : '#000' }}></div>
+      <div className='w-4 h-4 rounded-full' style={{ backgroundColor: (languageColors && languageColors[randomRepo.language]) ? languageColors[randomRepo.language].color : '#000' }}></div>
       <div>{randomRepo && randomRepo.language}</div>
     </div>
   )

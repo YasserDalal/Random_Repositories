36a563b811b6c4b76c667ed162d9695cb109b2df
YasserDalal <170532228+YasserDalal@users.noreply.github.com>
2025-07-17 21:16:45 +0300

    make the GuestButton component color a little bit darker

--- src/components/Modal/GuestButton.jsx
+++ src/components/Modal/GuestButton.jsx
@@ -5,7 +5,7 @@ export default function GuestButton({ className, fetchRepository }) {
   }
   return (
     <div className={className}>
-      <div className='bg-[#b9b9b9] rounded-xl py-2 px-4 w-full hover:bg-[#2a2a2a] hover:text-[#b9b9b9] cursor-pointer transition ease-in-out duration-100 text-center text-black text-xl font-semibold' typeof='button' onClick={handleClickGuest}>Continue as guest</div>
+      <div className='bg-[#494949] rounded-xl py-2 px-4 w-full hover:bg-[#3c3c3c] cursor-pointer transition ease-in-out duration-100 text-center text-[#dbdada] text-xl font-semibold' typeof='button' onClick={handleClickGuest}>Continue as guest</div>
     </div>
   )
 }
